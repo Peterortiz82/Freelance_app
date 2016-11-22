18fef52923f945ea7578098177da0e575db66cf7
Peter Ortiz <peterortiz@Peter-Ortiz-MBP-739.local>
2016-11-22 00:20:13 -0500

    finished proposal form

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -8,37 +8,39 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var app_component_1 = require('./app.component');
-var homepage_component_1 = require('./homepage/homepage.component');
-var documents_component_1 = require('./documents/documents.component');
-var proposal_list_component_1 = require('./proposal/proposal-list.component');
-var proposal_new_component_1 = require('./proposal/proposal-new.component');
-var app_routing_module_1 = require('./app-routing.module');
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms");
+var app_component_1 = require("./app.component");
+var homepage_component_1 = require("./homepage/homepage.component");
+var documents_component_1 = require("./documents/documents.component");
+var proposal_list_component_1 = require("./proposal/proposal-list.component");
+var proposal_new_component_1 = require("./proposal/proposal-new.component");
+var app_routing_module_1 = require("./app-routing.module");
 var AppModule = (function () {
     function AppModule() {
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                app_routing_module_1.AppRoutingModule
-            ],
-            declarations: [
-                app_component_1.AppComponent,
-                homepage_component_1.HomepageComponent,
-                documents_component_1.DocumentsComponent,
-                proposal_list_component_1.ProposalListComponent,
-                proposal_new_component_1.ProposalNewComponent
-            ],
-            bootstrap: [
-                app_component_1.AppComponent
-            ]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppModule);
     return AppModule;
 }());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [
+            platform_browser_1.BrowserModule,
+            app_routing_module_1.AppRoutingModule,
+            forms_1.FormsModule
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            homepage_component_1.HomepageComponent,
+            documents_component_1.DocumentsComponent,
+            proposal_list_component_1.ProposalListComponent,
+            proposal_new_component_1.ProposalNewComponent
+        ],
+        bootstrap: [
+            app_component_1.AppComponent
+        ]
+    }),
+    __metadata("design:paramtypes", [])
+], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
