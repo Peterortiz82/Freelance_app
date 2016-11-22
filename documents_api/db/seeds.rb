5c290775a75613216dbb500dc5497621e4ffe40b
Peter Ortiz <peterortiz@Peter-Ortiz-MBP-739.local>
2016-11-22 15:07:03 -0500

    generated fake data with seed file

--- documents_api/db/seeds.rb
+++ documents_api/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |doc|
+  FreelanceDocument.create!(title: "Document #{doc}",
+    description: 'Af live-edge gochujang, polaroid synth lo-fi umami retro hell of hammock helvetica mixtape meggings taxidermy twee. Gentrify affogato brunch af synth, keytar photo booth mixtape sartorial knausgaard. Gluten-free YOLO lo-fi yuccie, authentic cronut food truck raw denim disrupt ramps organic subway tile bicycle rights. Vexillologist iceland mustache squid cornhole authentic.',
+    file_url: "https://docs.google.com/document/d/1NQfYeMaxVqHsk0sDeAm2XLBjJdV5Nlx_dNfFn8oncso/edit?usp=sharing",
+    image_url: "https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg")
+end
+
