d5df4467e1141439ae1e0fe845e3b7f01af9a91f
Peter Ortiz <peterortiz@Peter-Ortiz-MBP-739.local>
2016-11-21 21:06:37 -0500

    set up routes for root and home

--- app/homepage/homepage.component.js
+++ app/homepage/homepage.component.js
@@ -8,18 +8,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require("@angular/core");
 var HomepageComponent = (function () {
     function HomepageComponent() {
     }
-    HomepageComponent = __decorate([
-        core_1.Component({
-            selector: 'homepage',
-            template: '<h1>Home/h1>'
-        }), 
-        __metadata('design:paramtypes', [])
-    ], HomepageComponent);
     return HomepageComponent;
 }());
+HomepageComponent = __decorate([
+    core_1.Component({
+        selector: 'homepage',
+        template: '<h1>Home</h1>'
+    }),
+    __metadata("design:paramtypes", [])
+], HomepageComponent);
 exports.HomepageComponent = HomepageComponent;
 //# sourceMappingURL=homepage.component.js.map
